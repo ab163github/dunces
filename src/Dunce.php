d465686403ebf6eafcc96b01421b8d324086b3f1
ab11 <ab__@163.com>
2017-12-18 18:28:09 +0800

    in develop

--- src/Dunce.php
+++ src/Dunce.php
@@ -17,15 +17,25 @@ class Dunce
     private $container;
 
     public $version = '0.0.1';
+
+    private function initContainer($Apps)
+    {
+        //TODO 由此开始折腾
+        $this->container = new Container();
+        $this->container['Dunce'] = $this; //框架本身 包含 配置信息 与 容器
+        $this->container['Console'] = new Console($this); //命令行脚本
+    }
     
     public function __construct($settings = array())
     {
         $defSetting = Config::load(__DIR__.'/Lib/Dunces.ini');
-//        var_dump($defSetting->all());
-//        die;
-        $this->container = new Container();
-        $this->container['Dunce'] = $this; //框架本身 包含 配置信息 与 容器
-        $this->container['Console'] = new Console($this); //命令行脚本
+        $defFwSetting = $defSetting->get('Apps',null);
+
+        $defConsoleSetting = $defSetting->get('Console');
+        $consoleName
+        var_dump($defConsoleSetting);
+        die;
+
 
     }
 
