41f76ece7107921c2d312f16d892bbd4bdea11e4
ab11 <ab__@163.com>
2017-12-26 18:35:15 +0800

    in develop

--- src/Exts/Services/SwBasedService.php
+++ src/Exts/Services/SwBasedService.php
@@ -20,7 +20,7 @@ class SwBasedService implements IConsoleManagementService
 
     public function start()
     {
-        $http = new \swoole_http_server("127.0.0.1", 80);
+        $http = new \swoole_http_server("0.0.0.0", 80);
         $http->on('request', function ($request, $response) {
             $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
         });
