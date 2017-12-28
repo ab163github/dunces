51427bf2d2595055c627408f19cbf1c31a097c85
ab11 <ab__@163.com>
2017-12-28 18:31:20 +0800

    in develop

--- src/Exts/Services/SwBasedService.php
+++ src/Exts/Services/SwBasedService.php
@@ -9,7 +9,7 @@
 namespace Dunces\Exts\Services;
 
 
-use Dunces\Exts\Console\Lib\ConsoleException;
+use Dunces\Dunce;
 use Dunces\Exts\Services\Lib\IConsoleManagementService;
 
 class SwBasedService implements IConsoleManagementService
@@ -17,54 +17,102 @@ class SwBasedService implements IConsoleManagementService
     private $pidPath;
     private $serverName;
 
-    private function writePid(){
+    private function getPidFileName(){
+        if(!is_dir($this->pidPath)) {
+            $mkdir = function($path,$f){
+                if(is_dir($path)) return true;
+                if(is_dir(dirname($path))){//父目录已经存在，直接创建
+                    return mkdir($path);
+                }
+                $f(dirname($path),$f);//从子目录往上创建
+                return mkdir($path,0777);//因为有父目录，所以可以创建路径
+            };
+            $mkdir($this->pidPath,$mkdir);
+        }
+        return $this->pidPath.'/'.$this->serverName.'.pid';
+    }
 
+    private function writePid($pid){
+        return file_put_contents($this->getPidFileName(),$pid);
     }
 
     private function loadPid(){
-
+        if(file_exists($this->getPidFileName()))
+            $pid = file_get_contents($this->getPidFileName());
+        else
+            $pid = 0;
+        return $pid;
     }
 
     public function __construct($serverName)
     {
         $this->serverName = $serverName;
         $settingName = Dunce::SETTING_NAME;
-        $this->pidPath = Dunce::$settingName()->get('Services.pid_path','\etc\Dunces\service');
+        $this->pidPath = Dunce::$settingName()->get('Services.pid_path','/etc/Dunces/Service');
     }
 
     public function start()
     {
-
-        if(empty($pidPath)) $pidPath = '\etc\Dunces\service';
-        $pidFile = $
-        $http = new \swoole_http_server("0.0.0.0", 80);
-        $http->set(array('daemonize'=>1));
-        $http->on('start',function($server){
-            $pid = $server->master_pid;
-            swoole_set_process_name("web_server_".$pid);
-        });
-        $http->on('managerStart',function ($server){
-            $pid = $server->master_pid;
-            swoole_set_process_name("web_server_manager".$pid);
-        });
-        $http->on('workerStart',function ($server,$worker_id){
-            $pid = $server->master_pid;
-            swoole_set_process_name("web_server_".$pid."_worker_".$worker_id);
-        });
-        $http->on('request', function ($request, $response) {
-            $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
-        });
-        $http->start();
+        if($this->loadPid() == 0){
+            $http = new \swoole_http_server("0.0.0.0", 80);
+            $http->set(array('daemonize'=>1));
+            $sobj = $this;
+            $http->on('start',function($server) use ($sobj){
+                $pid = $server->master_pid;
+                swoole_set_process_name("web_server_".$pid);
+                if($sobj->writePid($pid)){
+                    //echo $pid;
+                }else{
+                    $server->shutdown();
+                };
+            });
+            $http->on('shutdown', function ($server) use ($sobj) {
+                $sobj->writePid(0);
+            });
+            $http->on('managerStart',function ($server){
+                $pid = $server->master_pid;
+                swoole_set_process_name("web_server_manager".$pid);
+            });
+            $http->on('workerStart',function ($server,$worker_id){
+                $pid = $server->master_pid;
+                swoole_set_process_name("web_server_".$pid."_worker_".$worker_id);
+            });
+            $http->on('request', function ($request, $response) {
+                $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
+            });
+            $http->start();
+        };
     }
 
     public function stop()
     {
         // TODO: Implement stop() method.
+        $timeout = 60;
+        $startTime = time();
+        $this->serverSetting['masterPid'] && posix_kill($this->serverSetting['masterPid'], SIGTERM);
+
+        $result = true;
+        while (1) {
+            $masterIslive = $this->serverSetting['masterPid'] && posix_kill($this->serverSetting['masterPid'], SIGTERM);
+            if ($masterIslive) {
+                if (time() - $startTime >= $timeout) {
+                    $result = false;
+                    break;
+                }
+                usleep(10000);
+                continue;
+            }
+
+            break;
+        }
+        return $result;
     }
 
     public function reload()
     {
         // TODO: Implement reload() method.
+        $signal = $onlyTask ? SIGUSR2 : SIGUSR1;
+        posix_kill($this->serverSetting['managerPid'], $signal);
     }
 
     public function restart()
